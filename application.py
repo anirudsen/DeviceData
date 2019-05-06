20a0082f29b31e3dfe14c8d49f8ae4069fcdaa64
anirud <anirudsen@gmail.com>
2019-05-06 21:16:25 +0530

    removed harded coded value

--- application.py
+++ application.py
@@ -75,7 +75,7 @@ def getData():
 	    sql_query = "SELECT "+ columnname + "FROM "+ tablename +" WHERE Last_Update_Date >='" + incrementaldate + "';"
 
     '''
-    cursor.execute("SELECT TOP 10 * FROM dbo.iSolve_Device_Data_Stg;") 
+    cursor.execute(sql_query) 
     #rows = cursor.fetchall()
     columns = [column[0] for column in cursor.description]
     results = []
